13b693a42c912829262d5f334b4528e0e9dba58a
fengmingjian <fmj@hanzhisoft.com>
2023-04-25 21:21:32 +0800

    feat: init search

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,28 +1,41 @@
-import { defineConfig } from 'vitepress'
-
+import { defineConfig } from "vitepress";
+import path from "node:path";
+import { SearchPlugin } from "vitepress-plugin-search";
 // https://vitepress.dev/reference/site-config
+
 export default defineConfig({
+  outDir: path.resolve(__dirname, "../dist"),
   title: "aymoc写博客的地方",
   description: "博客",
+  vite: {
+    plugins: [
+      SearchPlugin({
+        previewLength: 62,
+        tokenize: "full",
+        buttonLabel: "文档搜索",
+        placeholder: "请输入关键字...",
+      }),
+    ],
+  },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: "Examples",
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
